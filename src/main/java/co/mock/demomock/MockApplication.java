e159bcff17cc91844951799ff573c345c3678202
simonecordisco <mnlsmncrd@gmail.com>
2024-02-05 15:06:19 +0100

    Initial commit

---
+++ src/main/java/co/mock/demomock/MockApplication.java
@@ -0,0 +1,13 @@
+package co.mock.demomock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockApplication.class, args);
+	}
+
+}
---
+++ src/main/java/co/mock/demomock/entities/User.java
@@ -0,0 +1,25 @@
+package co.mock.demomock.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(unique = true, name ="email")
+    private String email;
+}
